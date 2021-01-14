67438f58828fd9117cea5e10fa10f5a9c81b1bcd
TheoMoriceEM <theo.morice5@gmail.com>
2021-01-14 23:12:26 +0100

    le vaisseau ne doit pas sortir de l’écran

---
+++ Assets/LimitScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitScreen : MonoBehaviour
+{
+    Camera cam;
+
+    float height;
+    float width;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+
+        height = cam.orthographicSize;
+        width = height * cam.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= width)
+        {
+            transform.position = new Vector3(width, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -width)
+        {
+            transform.position = new Vector3(-width, transform.position.y, 0);
+        }
+    }
+}
